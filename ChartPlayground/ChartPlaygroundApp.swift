c3efe51dfa9146305d02833f5aa487dd2a41a719
natecc <r7895566@gmai.com>
2022-07-25 17:50:30 +0800

    Initial Commit

---
+++ ChartPlayground/ChartPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartPlaygroundApp.swift
+//  ChartPlayground
+//
+//  Created by Cc C on 2022/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
